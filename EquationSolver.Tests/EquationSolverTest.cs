895d5025edcd958f888b7aac488cd68278b3eb5a
dmitry_meshkov@outlook.com <dmitry_meshkov@outlook.com>
2022-12-01 20:25:05 +0300

    FunctionalTest

--- EquationSolver.Tests/EquationSolverTest.cs
+++ EquationSolver.Tests/EquationSolverTest.cs
@@ -11,7 +11,7 @@ namespace EquationSolver.Tests
             var result = QuadraticEquationSolver.Solve(a, b, c);
 
             Assert.AreEqual(expectedResult.Length, result.Length);
-            for(int i = 0; i < result.Length; i++)
+            for (int i = 0; i < result.Length; i++)
                 Assert.AreEqual(expectedResult[i], result[i]);
         }
         [TestMethod]
@@ -24,6 +24,27 @@ namespace EquationSolver.Tests
         public void NegativeDiscriminant()
         {
             TestEquiation(1, 1, 1);
+        }
+
+        [TestMethod]
+        public void ZeroDiscriminant()
+        {
+            TestEquiation(1, 2, 1, -1);
+        }
+
+        [TestMethod]
+        public void FunctionalTest()
+        {
+            Random rand = new Random(1);
+            double a = rand.NextDouble() * 10;
+            double b = rand.NextDouble() * 10;
+            double c = rand.NextDouble() * 10;
+
+            var result = QuadraticEquationSolver.Solve(a, b, c);
+            foreach(var x in result)
+            {
+                Assert.AreEqual(0, a * x * x + b * x + c, 1e-10);
+            }
         }
     }
 }
